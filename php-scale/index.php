9c10b082cc606f0611b2b1c0e5adf4bc513e06f8
Bhumika-02 <bhumika.gurjar23@pcu.edu.in>
2025-01-22 00:00:42 +0530

    Added comments to index.php for scaling context

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,3 +1,5 @@
 <?php
+  // This line prints the hostname of the server
+  // It helps in identifying which server is handling the request in a scaled environment.
   print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>
